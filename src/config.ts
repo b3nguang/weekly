69e3f442073d564c19299ad2e9cd4cb804aeca30
b3nguang <1127786222@qq.com>
2024-10-28 22:19:30 +0800

    :sparkles: newpost1

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  title: '潮流周刊',
+  author: 'b3nguang',
+  description: '记录 b3nguang 的不枯燥生活，每周一发布，欢迎关注',
+  keywords: 'b3nguang,Weekly,Blog',
+  icon: 'https://b3nguang.oss-cn-hangzhou.aliyuncs.com/static/logo.png',
+  pic: 'https://b3nguang.oss-cn-hangzhou.aliyuncs.com/static/logo.png',
+  homePage: 'https://weekly.b3nguang.top',
+  blogPage: 'https://blog.b3nguang.top/',
+  twitterId: 'b3nguang',
+  githubId: 'b3nguang',
+  repo: 'b3nguang/weekly',
+};
